0184c0c7dc00a2c4db811e1f4c8e2d1956007865
Martyna-Tu <martyna.tucholska.wkr@gmail.com>
2022-04-15 23:00:54 +0200

    last modifications

--- pythonCalculator/main.py
+++ pythonCalculator/main.py
@@ -21,20 +21,12 @@ def divide(x, y):
     return x / y
 
 if __name__ == "__main__":
-    #print("Select operation.")
-    #print("1.Add")
-    #print("2.Subtract")
-    #print("3.Multiply")
-    #print("4.Divide")
 
     while True:
         # take input from the user
-        #choice = input("Enter choice(Add/Sub/Div/Mul): ")
         choice = sys.argv[1]
         # check if choice is one of the four options
         if choice in ('Add', 'Sub', 'Div', 'Mul'):
-            #num1 = float(input("Enter first number: "))
-            #num2 = float(input("Enter second number: "))
             num1 = float(sys.argv[2])
             num2 = float(sys.argv[3])
 
@@ -50,12 +42,5 @@ if __name__ == "__main__":
             elif choice == 'Div':
                 print(num1, "/", num2, "=", divide(num1, num2))
 
-            # check if user wants another calculation
-            # break the while loop if answer is no
-            #next_calculation = input("Let's do next calculation? (yes/no): ")
-            #if next_calculation == "no":
             break
-
-        else:
-            print("Invalid Input")
 
