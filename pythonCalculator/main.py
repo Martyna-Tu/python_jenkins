ab6dc74b6fc0746813cd2066c4ce0a1cc713c700
Martyna-Tu <martyna.tucholska.wkr@gmail.com>
2022-04-15 19:53:34 +0200

    small modification of calculator

--- pythonCalculator/main.py
+++ pythonCalculator/main.py
@@ -28,30 +28,30 @@ if __name__ == "__main__":
 
     while True:
         # take input from the user
-        choice = input("Enter choice(1/2/3/4): ")
+        choice = input("Enter choice(Add/Sub/Div/Mul): ")
 
         # check if choice is one of the four options
-        if choice in ('1', '2', '3', '4'):
+        if choice in ('Add', 'Sub', 'Div', 'Mul'):
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
 
-            if choice == '1':
+            if choice == 'Add':
                 print(num1, "+", num2, "=", add(num1, num2))
 
-            elif choice == '2':
+            elif choice == 'Sub':
                 print(num1, "-", num2, "=", subtract(num1, num2))
 
-            elif choice == '3':
+            elif choice == 'Mul':
                 print(num1, "*", num2, "=", multiply(num1, num2))
 
-            elif choice == '4':
+            elif choice == 'Div':
                 print(num1, "/", num2, "=", divide(num1, num2))
 
             # check if user wants another calculation
             # break the while loop if answer is no
-            next_calculation = input("Let's do next calculation? (yes/no): ")
-            if next_calculation == "no":
-                break
+            #next_calculation = input("Let's do next calculation? (yes/no): ")
+            #if next_calculation == "no":
+            break
 
         else:
             print("Invalid Input")
